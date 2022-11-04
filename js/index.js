aba166110959bdb6b3070d52a03a28106b1f89ef
Gerard Solanes Hernandez <gerardsolher@gmail.com>
2022-11-04 17:44:18 +0100

    Refactor iteration 1 with functions

--- js/index.js
+++ js/index.js
@@ -1,15 +1,30 @@
 // Iteration 1: Names and Input
-hacker1 = `Ton`;
-console.log(`The driver's name is ${hacker1}`);
-hacker2 = `Ton`;
-console.log(`The navigator's name is ${hacker2}`);
+const hacker1 = 'Gerard';
+const hacker2 = 'Cris';
+const nameProgrammers = function (driver, navigator) {
+    console.log(`The driver's name is ${driver}`);
+    console.log(`The navigator's name is ${navigator}`);
+}
 
 // Iteration 2: Conditionals
 if (hacker1.length > hacker2.length) {
     console.log(`Driver has the longest name, it has ${hacker1.length} characters`);
 } else if (hacker2.length > hacker1.length) {
-    console.log(`Navigator has the longest name, it has ${hacker2.length} characters`);
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
 } else {
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
 }
 // Iteration 3: Loops
+let capitalizedName = '';
+for (const letter of hacker1) {
+    capitalizedName += `${letter.toUpperCase()} `;
+}
+console.log(capitalizedName);
+
+let nameReverse = '';
+for (let letterIdx = hacker2.length-1; letterIdx >= 0; letterIdx--) {
+    nameReverse += hacker2[letterIdx];
+}
+console.log(nameReverse);
+
+
