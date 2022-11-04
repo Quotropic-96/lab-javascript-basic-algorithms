7e0a140d6000aa1fe59e0a07a582e0eea40f5bbc
Gerard Solanes Hernandez <gerardsolher@gmail.com>
2022-11-04 18:21:35 +0100

    Finish iteration 3. Add 3 main functions for last exercise

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Iteration 1: Names and Input
-const hacker1 = 'Gerard';
-const hacker2 = 'Cris';
+const hacker1 = 'Aaag';
+const hacker2 = 'Aaagc';
 const nameProgrammers = function (driver, navigator) {
     console.log(`The driver's name is ${driver}`);
     console.log(`The navigator's name is ${navigator}`);
@@ -20,16 +20,68 @@ const compareProgrammersNameLength = (driver, navigator) => {
 compareProgrammersNameLength(hacker1, hacker2);
 
 // Iteration 3: Loops
-let capitalizedName = '';
-for (const letter of hacker1) {
-    capitalizedName += `${letter.toUpperCase()} `;
+function capitalizeName (nameStr) {
+    let capitalizedName = '';
+    for (const letter of nameStr) {
+        capitalizedName += `${letter.toUpperCase()} `;
+    }
+    return capitalizedName;
 }
-console.log(capitalizedName);
+console.log(capitalizeName(hacker1));
 
-let nameReverse = '';
-for (let letterIdx = hacker2.length-1; letterIdx >= 0; letterIdx--) {
-    nameReverse += hacker2[letterIdx];
+function reverseName (nameStr) {
+     let reversedName = '';
+    for (let letterIdx = nameStr.length-1; letterIdx >= 0; letterIdx--) {
+        reversedName += nameStr[letterIdx];
+    }
+    return reversedName;
 }
-console.log(nameReverse);
+console.log(reverseName(hacker2));
 
-
+function computeLengthOfShortestName (driver, navigator) {
+    if (driver.length > navigator.length) {
+        return navigator.length;
+    } else {
+        return driver.length;
+    }
+}
+function computeFirstProgrammer (driver, navigator) {
+    const shortestName = computeLengthOfShortestName(driver, navigator); 
+    for (let i = 0; i < shortestName; i++) {
+        if (driver[i] == navigator[i]) {
+            continue;
+        }
+        if (driver[i] > navigator[i]) {
+            return navigator;
+        }
+        if (driver[i] < navigator[i]) {
+            return driver;
+        }
+    }
+    if (driver.length === navigator.length) {
+        return 'equal';
+    }
+    if (driver.length < navigator.length) {
+        return driver;
+    }
+    if (driver.length > navigator.length) {
+        return navigator;
+    }
+}
+function outputResult (driver, navigator) {
+    const firstProgrammer = computeFirstProgrammer(driver, navigator);
+    switch (firstProgrammer) {
+        case driver:
+           console.log("The driver's name goes first.");
+           break;
+        case navigator:
+            console.log("Yo, the navigator goes first definitely.");
+            break;
+        case 'equal':
+            console.log("What?! You both have the same name?");
+            break;
+        default:
+            console.log('Ups... Something went wrong');
+    }
+}
+outputResult(hacker1, hacker2);
